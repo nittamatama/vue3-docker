ae34ce528042926c46973eeacf0634ec4c3b83fa
nittamatama <blackmnshin@gmail.com>
2023-08-11 14:33:13 +0900

    v-onディレクティブ

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,52 +1,20 @@
 <script setup lang="ts">
-  import { ref, computed } from "vue";
+  import { ref } from "vue";
 
-  const msg = ref("こんにちは世界");
-  const msgTextRed = ref("red");
-  const msgBigColor = ref("black");
-  const msgTextColor = ref("white");
-  const msgStyles = ref({
-    color: "white",
-    backgroundColor: "black"
-  });
-  const msgStyles2 = ref({
-    fontSize: "24pt"
-  });
-  const msgStyles3 = ref({
-    color: "pink",
-    fontSize: "24pt"
-  });
-  const textSize = computed(
-    (): string => {
-      const size = Math.round(Math.random() * 25) + 10;
-      return `${size}pt`;
-    }
-  );
+  const randValue = ref("まだです");
+  const onButtonClick = (): void => {
+    const rand = Math.round(Math.random() * 10);
+    randValue.value = String(rand);
+  };
+
 </script>
 
 <template>
-  <p v-bind:style="{color: msgTextRed}">
-  {{ msg }}
-  </p>
-  <p v-bind:style="{color: 'pink'}">
-  {{ msg }}
-  </p>
-  <p v-bind:style="{fontSize: textSize}">
-  {{ msg }}
-  </p>
-  <p v-bind:style="{color: msgTextColor, backgroundColor: msgBigColor}">
-  {{ msg }}
-  </p>
-  <p v-bind:style="{color: msgTextRed}">
-  {{ msg }}
-  </p>
-  <p v-bind:style="{color: msgTextRed}">
-  {{ msg }}
-  </p>
-  <p v-bind:style="{color: msgTextRed}">
-  {{ msg }}
-  </p>
-  <p v-bind:style="{color: msgTextRed}">
-  {{ msg }}
-  </p>
+  <section>
+    <button v-on:click="onButtonClick">クリック</button>
+    <p>クリック結果: {{ randValue }}</p>
+  </section>
 </template>
+
+<style>
+</style>
