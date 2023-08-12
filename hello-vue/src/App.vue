e05b3a7031421d461b4275a7e90233f5508bc393
nittamatama <blackmnshin@gmail.com>
2023-08-12 13:30:38 +0900

    click関連の修飾子

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
   import { ref } from "vue";
 
-  const msg = ref("未送信");
-  const onFormSubmit = (): void => {
-    msg.value = "送信しました";
+  const msg = ref("まだです");
+  const onEnterKey = (): void => {
+    msg.value = "エンター気ーのうか";
+  };
+  const onRightButtonClick = (): void => {
+    msg.value = "ボタンが右クリックされました";
+  };
+  const onShiftClick = (): void => {
+    msg.value = "shiftキーを押しながらクリックされた";
   };
 </script>
 
 <template>
-  <form action="#" v-on:submit.prevent="onFormSubmit">
-    <input type="text" required>
-    <button type="submit">送信</button>
-  </form>
   <p>{{ msg }}</p>
+  <input type="text" v-on:keydown.enter="onEnterKey"><br>
+  <button v-on:click.right="onRightButtonClick">右クリック</button><br>
+  <button v-on:click.shift="onShiftClick">shiftおしながら</button><br>
 </template>
 
 <style>
