5715474e92c7b4d6fa929ddfd8ca2ccb61667d76
nittamatama <blackmnshin@gmail.com>
2023-08-15 08:31:01 +0900

    v-bindが出力でv-onが入力みたいな雑な覚え方

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
   import { ref } from "vue";
 
-  const inputNameModel = ref("双方向");
+  const inputName2Way = ref("双方向");
+  const onInputName2Way = (event: Event): void => {
+    const element = event.target as HTMLInputElement;
+    inputName2Way.value = element.value;
+  };
 </script>
 
 <template>
   <section>
-    <input type="text" v-model="inputNameModel">
-    <p>{{ inputNameModel }}</p>
+    <input type="text" v-bind:value="inputName2Way" v-on:input="onInputName2Way">
+    <p>{{ inputName2Way }}</p>
   </section>
 </template>
 
